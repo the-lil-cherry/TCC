4d4dfacb55eac6979050038204ecdb76bcc50a5c
Anna <annabserikyaku@gmail.com>
2026-05-04 07:58:03 -0300

    Add index in each node of PQ

--- priorityQueue.cpp
+++ priorityQueue.cpp
@@ -17,7 +17,9 @@ class MinPQ{
         void exch(long long i, long long j){
             Path * t = pq[i];
             pq[i] = pq[j];
+            pq[i]->indexPQ = i;
             pq[j] = t;
+            pq[j]->indexPQ = j;
         }
 
         MinPQ(){
@@ -82,21 +84,24 @@ class MinPQ{
 int main(){
     MinPQ test;
     Path * first = new Path(1, 2, 10, nullptr, nullptr, 10);
+    first->indexPQ = 1;
     test.insert(first);
 
     Path * second = new Path(1,2,5, nullptr, nullptr, 10);
+    second->indexPQ = 2;
     test.insert(second);
 
     Path * third = new Path(1,3,20, nullptr, nullptr, 10);
+    third->indexPQ = 3;
     test.insert(third);
 
     Path * fourth = new Path(1,2,1, nullptr, nullptr, 10);
+    third->indexPQ = 4;
     test.insert(fourth);
 
     while(!test.isEmpty()){
         cout << test.min()->weight << endl;
         test.remove(1);
-        cout << endl;
     }
 
 }
